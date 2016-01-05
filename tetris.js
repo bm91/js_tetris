67f481a7083b9ba840ce8b1174cbf80c680edeb7
Benedikt <bm91@users.noreply.github.com>
2016-01-05 22:09:04 +0100

    worked on block rotation, still need to look into strange block behaviour whilst movement of blocks

--- tetris.js
+++ tetris.js
@@ -8,8 +8,6 @@ window.addEventListener("resize", resizecontrol);
 num_x = 14; //number of columns;
 num_y = 20; //number of rows;
 
-blockfieldupdate = false;
-
 tiles = new Array();
 
 current_tile = {};
@@ -19,7 +17,7 @@ field = new Array(num_x*num_y);
 uf = setInterval(updateField, 100);
 block_uf = false;
 ltfd = setInterval(letTilesFallDown, 1000);
-block_ltft = false;
+block_ltfd = false;
 
 for(var i = 0;i < num_y;i++){
   field[i] = new Array(num_x);
@@ -31,30 +29,58 @@ for(var i = 0;i < num_y;i++){
   }
 }
 
+blockcolors = new Array();
+
 function Block (id, position, color){
-    var structure = [[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]]; //0 means not set, 1 means set
+    var structure = [[[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]],
+                     [[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]],
+                     [[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]],
+                     [[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]]];
+
+    blockstate = 0;
 
     switch(id){
       case 0:
-        structure = [[1,1,1,1], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[0] = [[1,1,1,1], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[1] = [[1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0]];
+        structure[2] = [[1,1,1,1], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[3] = [[1,0,0,0], [1,0,0,0], [1,0,0,0], [1,0,0,0]];
         break;
       case 1: // --
-        structure = [[1,1,1,0], [0,0,1,0], [0,0,0,0], [0,0,0,0]];
+        structure[0] = [[1,1,1,0], [0,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[1] = [[1,0,0,0], [1,1,0,0], [1,0,0,0], [0,0,0,0]];
+        structure[2] = [[0,0,0,0], [0,1,0,0], [1,1,1,0], [0,0,0,0]];
+        structure[3] = [[0,0,1,0], [0,1,1,0], [0,0,1,0], [0,0,0,0]];
         break;
       case 2: // -|
-        structure = [[1,1,1,0], [1,0,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[0] = [[1,1,1,0], [1,0,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[1] = [[1,0,0,0], [1,0,0,0], [1,1,0,0], [0,0,0,0]];
+        structure[2] = [[0,0,1,0], [1,1,1,0], [0,0,0,0], [0,0,0,0]];
+        structure[3] = [[1,1,0,0], [0,1,0,0], [0,1,0,0], [0,0,0,0]];
         break;
-      case 3: // |-
-        structure = [[1,1,0,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
+      case 3: // []
+        structure[0] = [[1,1,0,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[1] = [[1,1,0,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[2] = [[1,1,0,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[3] = [[1,1,0,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
         break;
-      case 4: // []
-        structure = [[0,1,1,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
+      case 4: // |-
+        structure[0] = [[0,1,1,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[1] = [[1,0,0,0], [1,1,0,0], [0,1,0,0], [0,0,0,0]];
+        structure[2] = [[0,1,1,0], [1,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[3] = [[1,0,0,0], [1,1,0,0], [0,1,0,0], [0,0,0,0]];
         break;
       case 5: // -|-
-        structure = [[1,1,1,0], [0,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[0] = [[1,1,1,0], [0,1,0,0], [0,0,0,0], [0,0,0,0]];
+        structure[1] = [[1,0,0,0], [1,1,0,0], [1,0,0,0], [0,0,0,0]];
+        structure[2] = [[0,0,0,0], [0,1,0,0], [1,1,1,0], [0,0,0,0]];
+        structure[3] = [[0,0,1,0], [0,1,1,0], [0,0,1,0], [0,0,0,0]];
         break;
       case 6: //--__
-        structure = [[1,1,0,0], [0,1,1,0], [0,0,0,0], [0,0,0,0]];
+        structure[0] = [[1,1,0,0], [0,1,1,0], [0,0,0,0], [0,0,0,0]];
+        structure[1] = [[0,1,0,0], [1,1,0,0], [1,0,0,0], [0,0,0,0]];
+        structure[2] = [[1,1,0,0], [0,1,1,0], [0,0,0,0], [0,0,0,0]];
+        structure[3] = [[0,1,0,0], [1,1,0,0], [1,0,0,0], [0,0,0,0]];
         break;
       default: //error
         structure = [[1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1]];
@@ -62,6 +88,7 @@ function Block (id, position, color){
 
     this.position = position;
     this.structure = structure;
+    this.blockstate = blockstate;
     this.color = color;
     this.blocked = false;
 
@@ -70,6 +97,10 @@ function Block (id, position, color){
 }
 
 function init(){
+
+  for(var i = 0;i < 7;i++) // define colors for every block type
+    blockcolors[i] = getrandomcolor();
+
   resizecontrol();
   addTiles();
 }
@@ -155,7 +186,7 @@ function updateField(){
   for(var i = 0;i < tiles.length;i++){
     for(var x = 0;x < 4;x++){
       for(var y = 0;y < 4;y++){
-        if(tiles[i].structure[x][y] == 1){ //check the pieces of a block and set the tiles where pieces are located
+        if(tiles[i].structure[tiles[i].blockstate][x][y] == 1){ //check the pieces of a block and set the tiles where pieces are located
           try{
             t[tiles[i].position[0] + num_x * tiles[i].position[1] + x + num_x * y].style.backgroundColor = tiles[i].color;
             field[tiles[i].position[0] + x][tiles[i].position[1] + y] = 1;
@@ -184,7 +215,8 @@ function updateField(){
 
   if(getnewblock == true){
     checkIfLineCompleted();
-    tiles[tiles.length] = new Block(parseInt(Math.random() * 7), [parseInt(num_x / 2), -4], randCol());
+    var n = parseInt(Math.random() * 7);
+    tiles[tiles.length] = new Block(n, [parseInt(num_x / 2), -4], blockcolors[n]);
   }
   //end of new block check
 
@@ -193,7 +225,7 @@ function updateField(){
 function checkIfLineCompleted(){
 
   block_uf = true;
-  block_ltft = true;
+  block_ltfd = true;
 
   for(var y = 0;y < num_y;y++){
     var linecompleted = true;
@@ -206,7 +238,7 @@ function checkIfLineCompleted(){
         for(var xx = 0;xx < 4;xx++){
           for(var yy = 0;yy < 4;yy++){
             if(tiles[i].position[1] + yy == y && tiles[i].structure[xx][yy] == 1){ //delete tiles out of affected blocks
-              tiles[i].structure[xx][yy] = 0;
+              tiles[i].structure[tiles[i].blockstate][xx][yy] = 0;
             }
           }
         }
@@ -215,21 +247,21 @@ function checkIfLineCompleted(){
   }
 
   block_uf = false;
-  block_ltft = false;
+  block_ltfd = false;
 
 }
 
 function letTilesFallDown(){
 
-  if(block_ltft == true)
+  if(block_ltfd == true)
     return;
 
   for(var i = 0;i < tiles.length;i++){
     for(var x = 0;x < 4; x++){
       for(var y = 0;y < 4;y++){
 
-        if(tiles[i].blocked == false && tiles[i].structure[x][y] == 1){
-          if(field[tiles[i].position[0] + x][tiles[i].position[1] + y + 1] == 1 && (y == 3 || tiles[i].structure[x][y + 1] == 0))
+        if(tiles[i].blocked == false && tiles[i].structure[tiles[i].blockstate][x][y] == 1){
+          if(field[tiles[i].position[0] + x][tiles[i].position[1] + y + 1] == 1 && (y == 3 || tiles[i].structure[tiles[i].blockstate][x][y + 1] == 0))
             tiles[i].blocked = true;
           if(tiles[i].position[1] + 1 + y >= num_y)
             tiles[i].blocked = true;
@@ -244,7 +276,7 @@ function letTilesFallDown(){
 
 function moveTilesHor(direction){
 
-  block_ltft = true;
+  block_ltfd = true;
   block_uf = true;
 
     if(direction == "left"){
@@ -255,7 +287,7 @@ function moveTilesHor(direction){
         for(var y = 0;y < 4;y++){
           try{
             if(field[tiles[tiles.length - 1].position[0] + x - 1][tiles[tiles.length - 1].position[1] + y] == 1 // check if tile next to block is occupied
-                && tiles[tiles.length - 1].structure[x - 1][y] == 0) // and check if next tile is part of block
+                && tiles[tiles.length - 1].structure[tiles[tiles.length - 1].blockstate][x - 1][y] == 0) // and check if next tile is part of block
                   allow_movement = false;
           } catch(TypeError){}
         }
@@ -274,7 +306,7 @@ function moveTilesHor(direction){
         for(var y = 0;y < 4;y++){
           try{
           if(field[tiles[tiles.length - 1].position[0] + x + 1][tiles[tiles.length - 1].position[1] + y] == 1 //check if tile next to block is occupied
-              && tiles[tiles.length - 1].structure[x + 1][y] == 0) // and check if next tile is part of block
+              && tiles[tiles.length - 1].structure[tiles[tiles.length - 1].blockstate][x + 1][y] == 0) // and check if next tile is part of block
                 allow_movement = false;
           } catch(TypeError) {}
         }
@@ -286,32 +318,48 @@ function moveTilesHor(direction){
         tiles[tiles.length - 1].position[0] += 1;
     }
 
-    block_ltft = false;
+    block_ltfd = false;
     block_uf = false;
 }
 
 function rotateTile(direction){
 
-    if(tiles[tiles.length - 1].blocked == true)
+    block_uf = true;
+    block_ltfd = true;
+
+    var current_tile = tiles[tiles.length - 1]
+
+    if(current_tile.blocked == true)
       return;
 
-    var cache = tiles[tiles.length - 1].structure;
-    var newstructure = [[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]];
+    var allow_rotation = true;
+
+    var newstructure = current_tile.structure[(current_tile.blockstate + 1) % 4];
+
     for(var x = 0;x < 4;x++){
       for(var y = 0;y < 4;y++){
-        newstructure[x][y] = cache[y][x];
+
+        //abort rotation if object is in the way or block would rotate out of gamefield
+        if(newstructure[x][y] == 1){ //check if rotation can be allowed
+          if(field[current_tile.position[0] + x][current_tile.position[1] + y] == 1 && current_tile.structure[x][y] == 0)
+            allow_rotation = false;
+
+          if(current_tile.position[0] + x >= num_x || current_tile.position[0] + x < 0) //check limits in horizontal direction
+            allow_rotation = false;
+
+          if(current_tile.position[1] + y >= num_y) //check limit in vertical direction
+            allow_rotation = false;
+        }
+
       }
     }
 
-    final_newstructure = [[0,0,0,0], [0,0,0,0], [0,0,0,0],[0,0,0,0]];
-    for(var x = 0;x < 4;x++){
-      for(var y = 0;y < 4;y++){
-        final_newstructure[x][y] = newstructure[3 - x][y];
-      }
+    if(allow_rotation == true){
+        tiles[tiles.length - 1].blockstate = (tiles[tiles.length - 1].blockstate + 1) % 4;
     }
 
-
-    tiles[tiles.length - 1].structure = final_newstructure;
+    block_uf = false;
+    block_ltfd = false;
 }
 
 function trackkeys(e) {
@@ -361,14 +409,15 @@ function trackkeys(e) {
 
 }
 
-function randCol () {
+function getrandomcolor () {
 
-  var ch = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
+  //var ch = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F");
+  var ch = new Array("0","1","2","3","4","5","6","7","8","9");
 
   var cl = new Array(6);
 
   for(var a = 0;a < 6;a++) {
-  cl[a] = ch[Math.floor(Math.random() * 16)];
+  cl[a] = ch[Math.floor(Math.random() * 10)];
   }
 
   return "#" + cl[0] + cl[1] + cl[2] + cl[3] + cl[4] + cl[5];
